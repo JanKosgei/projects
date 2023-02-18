c5667031e7210ca59a83c34f53c8b5ae0b7318f9
JanKosgei <januariusemm@gmail.com>
2023-02-18 12:37:49 +0300

    initial commit

---
+++ example.py
@@ -0,0 +1,61 @@
+import ipykernel
+
+
+print("Hello World!")
+
+
+2 + 2
+
+45/3
+
+3**2
+
+5**3
+
+
+55-5
+
+
+xx=9
+
+x=xx-2
+
+print(x)
+
+
+b=36
+
+
+x+b
+
+b/x
+
+
+m=1+2**3/4*5
+
+print(m)
+
+
+januel='jan'+'emmanuel'
+
+print(januel)
+
+sport='foot' + 'ball'
+
+print(sport)
+
+
+print(float(51)+ 49)
+
+jina=input('Who am I?')
+
+print('welcome',jina)
+
+team=input('Favourite club?')
+
+print('Team kubwa',team)
+
+inp=input('Eurpean Floor?')
+usf=int(inp)+1
+
+print('US Floor',usf)
